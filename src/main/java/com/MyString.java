5b46a1e6f477388f1cc51e7f89ce8464579289e3
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2018-12-26 16:35:05 +0200

    1

--- src/main/java/com/MyString.java
+++ src/main/java/com/MyString.java
@@ -11,31 +11,6 @@ public class MyString {
     private static final int ASCII_VALUE_OF_ZERO = 48;
 
 
-    /* public String getTheLongestContinuosChainOfOnesAndZeros(String inputData) {
-         String infoAboutOnes;
-         String infoAboutZeros;
-
-         ArrayList<Integer> indexesOfOnes = getIndexesOfOnesFromString(inputData);
-         ArrayList<Integer> indexesOfZeros = getIndexesOfZesoFromString(inputData);
-         if (!indexesOfOnes.isEmpty()) {
-             List<List> groupIndexesOfOnes = getGroupsOfIndexes(indexesOfOnes);
-             List<Integer> arrayListOfOnes = getTheLongestArrayList(groupIndexesOfOnes);
-             infoAboutOnes = "(" + arrayListOfOnes.get(0) + "," + arrayListOfOnes.get(arrayListOfOnes.size() - 1) + ")" + " цепочка единиц";
-         } else {
-             infoAboutOnes = " There aren't One's in string ";
-         }
-         if (!indexesOfZeros.isEmpty()) {
-             List<List> groupIndexesOfZeros = getGroupsOfIndexes(indexesOfZeros);
-             List<Integer> arrayListOfZeros = getTheLongestArrayList(groupIndexesOfZeros);
-             infoAboutZeros = "(" + arrayListOfZeros.get(0) + "," + arrayListOfZeros.get(arrayListOfZeros.size() - 1) + ")" + " цепочка нулей";
-         } else {
-             infoAboutZeros = " There aren't Zeros's in string";
-         }
-         logger.info(infoAboutOnes + infoAboutZeros);
-         return infoAboutOnes + infoAboutZeros;
-     }*/
-
-    ///////////////////////////////////////////////////////////////////////////////////////////
     public String getTheLongestContinuosChainOfOnesAndZeros(String inputData) {
         String infoAboutOnes;
         String infoAboutZeros;
@@ -60,7 +35,7 @@ public class MyString {
         return infoAboutOnes + infoAboutZeros;
     }
 
-///////////////////////////////////////////////////////////////////////////////////////////
+
     private ArrayList<Integer> getIndexesOfOnesFromString(String s) {
         ArrayList<Integer> indexesOfOnes = new ArrayList();
         for (int i = 0; i < s.length(); i++) {
@@ -89,7 +64,33 @@ public class MyString {
         }
         return listOfIndexes;
     }*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+    private List<List> getGroupsOfIndexes(ArrayList<Integer> listOfIndexes) {
 
+        List<List> listOfArrays = new ArrayList();
+        listOfIndexes.add(0);
+        for (int i = 0; i < listOfIndexes.size() - 1; i++) {
+            List<Integer> integers = new ArrayList<Integer>();
+            if (listOfIndexes.get(i).equals(listOfIndexes.get(i + 1) - 1)) {
+                for (int j = i; j < listOfIndexes.size() - 1; j++) {
+                    if (listOfIndexes.get(j).equals(listOfIndexes.get(j + 1) - 1)) {
+                        integers.add(listOfIndexes.get(j));
+                    } else if (!listOfIndexes.get(j).equals(listOfIndexes.get(j + 1) - 1)) {
+                        integers.add(listOfIndexes.get(j));
+                        i = j;
+                        break;
+                    }
+                }
+            } else {
+                integers.add(listOfIndexes.get(i));
+            }
+
+            listOfArrays.add(integers);
+        }
+        return listOfArrays;
+    }*/
+/////////////////////////////////////////////////////////////////////////////////
 
     private List<List> getGroupsOfIndexes(ArrayList<Integer> listOfIndexes) {
 
@@ -115,6 +116,10 @@ public class MyString {
         }
         return listOfArrays;
     }
+    ////////////////////////////////////////////////////////////////////////////
+
+
+
 
     public List<Integer> getTheLongestArrayList(List<List> list) {
         int maxSizeOfArray = 0;
