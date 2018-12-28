ac431a5ec8860167069ab88c349d2ba99527b497
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2018-12-28 16:32:04 +0200

    Finish tests1

--- src/test/java/com/NewMyStringTest.java
+++ src/test/java/com/NewMyStringTest.java
@@ -11,31 +11,35 @@ public class NewMyStringTest {
 
     @BeforeMethod
     public void setUp() {
-
         newMyStr = new NewMyString();
     }
 
     @Test
-    public void testThat1() {
+    public void testThat() {
+        Assert.assertEquals(newMyStr.getTheLongestGroupAndPositionOnesAndZerosInString("23564879785233546"), " There aren't One's in string    There aren't Zeros in string ");
+    }
+    @Test
+    public void testThat0() {
+        Assert.assertEquals(newMyStr.getTheLongestGroupAndPositionOnesAndZerosInString(""), " There aren't One's in string    There aren't Zeros in string ");
+    }
 
+    @Test
+    public void testThat1() {
         Assert.assertEquals(newMyStr.getTheLongestGroupAndPositionOnesAndZerosInString("10"), "(0 , 0 ) One-s  (1 , 1 ) Zeros");
     }
 
     @Test
     public void testThat2() {
-
         Assert.assertEquals(newMyStr.getTheLongestGroupAndPositionOnesAndZerosInString("1111"), "(0 , 3 ) One-s   There aren't Zeros in string ");
     }
 
     @Test
     public void testThat3() {
-
         Assert.assertEquals(newMyStr.getTheLongestGroupAndPositionOnesAndZerosInString("00000"), " There aren't One's in string   (0 , 4 ) Zeros");
     }
 
     @Test
     public void testThat4() {
-
         Assert.assertEquals(newMyStr.getTheLongestGroupAndPositionOnesAndZerosInString("1110000110010"), "(0 , 2 ) One-s  (3 , 6 ) Zeros");
     }
 }
