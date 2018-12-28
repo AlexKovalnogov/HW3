ea6031e9b0d6fcf0b70544d6461ade854352a3a7
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2018-12-28 15:54:09 +0200

    Magic

--- src/main/java/com/NewMyString.java
+++ src/main/java/com/NewMyString.java
@@ -8,7 +8,26 @@ public class NewMyString {
     private static final int ASCII_VALUE_OF_ZERO = 48;
 
 
-    public String getIndexesOfOnesFromString(String s) {
+    public String getTheLongestGroupAndPositionOnesAndZerosInString(String s) {
+        String infoAboutOnes;
+        String infoAboutZeros;
+        if (isContainsOnesInString(s)) {
+            infoAboutOnes = getInformationAboutOnesInString(s);
+        }
+        else {
+            infoAboutOnes = " There aren't One's in string ";
+        }
+        if (isContainsZerosInString(s)) {
+            infoAboutZeros = getInformationAboutZerosInString(s);
+        }
+        else {
+            infoAboutZeros = " There aren't Zeros in string ";
+        }
+        return infoAboutOnes+"  "+infoAboutZeros;
+    }
+
+
+    private String getInformationAboutOnesInString(String s) {
         int currentIndex = 0;
         int length = 0;
         for (int i = 0; i < s.length() - 1; i++) {
@@ -20,8 +39,24 @@ public class NewMyString {
                 i = i + length - 1;
             }
         }
-        logger.info("(" + currentIndex + " , " + length + " ) One-s");
-        return "(" + currentIndex + " , " + length + " ) One-s";
+        logger.info("(" + currentIndex + " , " + (currentIndex+length-1) + " ) One-s");
+        return "(" + currentIndex + " , " + (currentIndex+length-1) + " ) One-s";
+    }
+
+    private String getInformationAboutZerosInString(String s) {
+        int currentIndex = 0;
+        int length = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == ASCII_VALUE_OF_ZERO) {
+                if (getSequenceLength(i, s) > length) {
+                    length = getSequenceLength(i, s);
+                    currentIndex = i;
+                }
+                i = i + length - 1;
+            }
+        }
+        logger.info("(" + currentIndex + " , " + (currentIndex+length-1) + " ) Zeros");
+        return "(" + currentIndex + " , " +(currentIndex+length-1) + " ) Zeros";
     }
 
     private int getSequenceLength(int currentIndex, String s) {
@@ -37,19 +72,35 @@ public class NewMyString {
         return currentLength;
     }
 
-    private void isContainsZerosInString(){
-
+    private boolean isContainsZerosInString(String inputData) {
+        boolean containZeros = false;
+        for (int i = 0; i < inputData.length() - 1; i++) {
+            if (inputData.charAt(i) == ASCII_VALUE_OF_ZERO) {
+                containZeros = true;
+                break;
+            }
+        }
+        return containZeros;
     }
-    private void isContainsOnesInString(){
 
+    private boolean isContainsOnesInString(String inputData) {
+        boolean containOne = false;
+
+        for (int i = 0; i < inputData.length() - 1; i++) {
+            if (inputData.charAt(i) == ASCII_VALUE_OF_ONE) {
+                containOne = true;
+                break;
+            }
+        }
+        return containOne;
     }
 
 
     public static void main(String[] args) {
 
         //   String s1 = "10010001110000001111";
-        String s1 = "1001111000111111";
-        new NewMyString().getIndexesOfOnesFromString(s1);
+        String s1 = "0000";
+        new NewMyString().getTheLongestGroupAndPositionOnesAndZerosInString(s1);
 
     }
 
